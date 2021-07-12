2c76e359a5659ce03668289d85e0f512a8a193bd
alexseymourdev <alexseymourdev@gmail.com>
2021-07-12 20:05:10 +0100

    Update script.js

--- module-2/final_project/start/scripts/script.js
+++ module-2/final_project/start/scripts/script.js
@@ -1,2 +1,11 @@
-let buttons = document.querySelectorAll('.filterButton');
-console.log(buttons);
+let arrButtons = document.querySelectorAll('.filterButton');
+
+for(counter=0; counter < arrButtons.length; counter++){
+    let currentButton = arrButtons[counter];
+    console.log(currentButton);
+    currentButton.addEventListener('click',showHideImages)
+}
+
+function showHideImages(event){
+    console.log(event.target);
+}
