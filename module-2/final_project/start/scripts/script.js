1c22870b20476fc69398af61f3bfbda23ded16fc
alexseymourdev <alexseymourdev@gmail.com>
2021-07-12 20:49:21 +0100

    finished JS for photography page

--- module-2/final_project/start/scripts/script.js
+++ module-2/final_project/start/scripts/script.js
@@ -1,11 +1,30 @@
 let arrButtons = document.querySelectorAll('.filterButton');
+// console.log(arrButtons);
+let arrImages = document.querySelectorAll('.animalImage');
+// console.log(arrImages);
 
 for(counter=0; counter < arrButtons.length; counter++){
     let currentButton = arrButtons[counter];
-    console.log(currentButton);
+    // console.log(currentButton);
     currentButton.addEventListener('click',showHideImages)
 }
 
 function showHideImages(event){
-    console.log(event.target);
+    let animalFilter = event.target.getAttribute('animal');
+    // console.log(animalFilter);
+    for(counter=0; counter < arrImages.length; counter++){
+        let currentImage = arrImages[counter];
+        // console.log(currentImage);
+        currentImage.style.display = 'none';
+        let blnDisplay = false;
+        if(currentImage.classList.contains(animalFilter)){
+            blnDisplay = true;
+        }
+        if(animalFilter == 'all'){
+            blnDisplay = true;
+        }
+        if(blnDisplay){
+            currentImage.style.display = 'inline';
+        }
+    }
 }
