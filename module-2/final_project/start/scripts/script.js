baa76c1b31d39b29f0c6b85c958c7b02fddc3a5a
alexseymourdev <alexseymourdev@gmail.com>
2021-07-12 21:01:04 +0100

    changing procedural code into an object

--- module-2/final_project/start/scripts/script.js
+++ module-2/final_project/start/scripts/script.js
@@ -1,30 +1,40 @@
-let arrButtons = document.querySelectorAll('.filterButton');
-// console.log(arrButtons);
-let arrImages = document.querySelectorAll('.animalImage');
-// console.log(arrImages);
-
-for(counter=0; counter < arrButtons.length; counter++){
-    let currentButton = arrButtons[counter];
-    // console.log(currentButton);
-    currentButton.addEventListener('click',showHideImages)
-}
-
-function showHideImages(event){
-    let animalFilter = event.target.getAttribute('animal');
-    // console.log(animalFilter);
-    for(counter=0; counter < arrImages.length; counter++){
-        let currentImage = arrImages[counter];
-        // console.log(currentImage);
-        currentImage.style.display = 'none';
-        let blnDisplay = false;
-        if(currentImage.classList.contains(animalFilter)){
-            blnDisplay = true;
+let objAnimalFilters = {
+    init:function(){
+        this.selectElements();
+        this.addEventListeners();
+    },
+    selectElements:function(){
+        this.arrButtons = document.querySelectorAll('.filterButton');
+        // console.log(arrButtons);
+        this.arrImages = document.querySelectorAll('.animalImage');
+        // console.log(this);
+    },
+    addEventListeners:function(){
+        _self = this;
+        for(counter=0; counter < this.arrButtons.length; counter++){
+            let currentButton = this.arrButtons[counter];
+            // console.log(currentButton);
+            currentButton.addEventListener('click',_self.showHideImages)
         }
-        if(animalFilter == 'all'){
-            blnDisplay = true;
-        }
-        if(blnDisplay){
-            currentImage.style.display = 'inline';
+    },
+    showHideImages:function(event){
+        let animalFilter = event.target.getAttribute('animal');
+        // console.log(animalFilter);
+        for(counter=0; counter < _self.arrImages.length; counter++){
+            let currentImage = _self.arrImages[counter];
+            // console.log(currentImage);
+            currentImage.style.display = 'none';
+            let blnDisplay = false;
+            if(currentImage.classList.contains(animalFilter)){
+                blnDisplay = true;
+            }
+            if(animalFilter == 'all'){
+                blnDisplay = true;
+            }
+            if(blnDisplay){
+                currentImage.style.display = 'inline';
+            }
         }
     }
 }
+objAnimalFilters.init();
